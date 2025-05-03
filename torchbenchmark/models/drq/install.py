8f122f6010ef5a2e625f7070738bc1502c9a6c9a
ropzislaw <1710085142@qq.com>
2025-05-03 15:57:52 +0800

    DELETE MODELS

--- torchbenchmark/models/drq/install.py
+++
@@ -1,6 +0,0 @@
-from utils import s3_utils
-from utils.python_utils import pip_install_requirements
-
-if __name__ == "__main__":
-    pip_install_requirements()
-    s3_utils.checkout_s3_data("MODEL_PKLS", "drq/obs.pkl", decompress=False)
--- torchbenchmark/models/nvidia_deeprecommender/install.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-from utils.python_utils import pip_install_requirements
-
-if __name__ == "__main__":
-    pip_install_requirements()
